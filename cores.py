426c31a9942241a188def53105442baa2b5f7662
Fabricio Wonzoski <wonzoskifabricio@gmail.com>
2026-06-28 17:42:56 -0300

    Atualização de arquivos

--- cores.py
+++ cores.py
@@ -1,3 +1,38 @@
+# ANSII
+# 
+#     style back
+#      |     |
+# \033[0;33;44m<código>\033[m
+#        |
+#       text
+# 
+# 🔹Style
+#     0 --> None
+#     1 --> Bold
+#     4 --> Underline
+#     7 --> Negative
+# .
+# 🔹Text
+#     30 --> Branco
+#     31 --> Vermelho
+#     32 --> Verde
+#     33 --> Amarelo
+#     34 --> Azul
+#     35 --> Majenta
+#     36 --> Ciano
+#     37 --> Cinza
+# .
+# 
+# 🔹Back
+#     40 --> Branco
+#     41 --> Vermelho
+#     42 --> Verde
+#     43 --> Amarelo
+#     44 --> Azul
+#     45 --> Majenta
+#     46 --> Ciano
+#     47 --> Cinza
+
 import os
 os.system('clear')
 
@@ -12,5 +47,14 @@ cinza = str('\033[37m')
 negrito = str('\033[1m')
 linhado = str('\033[4m')
 negativo = str('\033[7m')
+
+backBranco = str('\033[0;30;40m')
+backVemelho = str('\033[0;30;41m')
+backVerde = str('\033[0;30;42m')
+backAmarelo = str('\033[0;30;43m')
+backAzul = str('\033[0;30;44m')
+backMajenta = str('\033[0;30;45m')
+backCiano = str('\033[0;30;46m')
+backCinza = str('\033[0;30;47m')
 
 fimCor = str('\033[m')
--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-# 045 - Crie um programa que faça o computador jogar JOKENPÔ com você.
-import os, random
+# 057 - Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores M ou F. Caso esteja errado, peça a digitação novamente até ter um valor correto.
+import cores
 
-os.system('clear')
-
-compEscolhe = random.randint(1,3)
-
-userEscolhe = int(input('-'*20 + '\n\033[1;33m  1 - Pedra\033[m\n' \
-'\033[1;34m  2 - Papel\033[m\n' \
-'\033[1;35m  3 - Tesoura\033[m\n' +\
-'-'*20+'\n'))
-
-print(compEscolhe)
-if userEscolhe == compEscolhe:
-    print('\033[33mEmpate!\033[m')
-elif (userEscolhe == 1 and compEscolhe == 3) or (userEscolhe == 2 and compEscolhe == 1) or (userEscolhe == 3 and compEscolhe == 2):
-    print('\033[32mVitória!\033[m')
-elif (userEscolhe == 3 and compEscolhe == 1) or (userEscolhe == 1 and compEscolhe == 2) or (userEscolhe == 2 and compEscolhe == 3):
-    print('\033[31mDerrota!\033[m')
-else:
-    print('Opção inválida!')
+while True:
+    sexo = str(input('Digite o sexo [M/F]: ')).strip().upper()[0]
+    if sexo == 'M':
+        print('Masculino.')
+        break
+    elif sexo == 'F':
+        print('Feminino.')
+        break
+    else:
+        print(f'Valor {cores.backVemelho}não{cores.fimCor} corresponde ao solicitado!\n')
+        continue
